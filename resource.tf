fb97703eadfc4d4468ab82454002491bd0ac031d
Vedantksahu <97789974+Vedantksahu@users.noreply.github.com>
2025-01-22 22:01:49 +0530

    Update resource.tf
    resource file

--- resource.tf
+++ resource.tf
@@ -1,7 +1,181 @@
-resource "aws_instance" "My_instance" {
-  ami = "xyz"
-  type = "t2.micro"
-  subnet_id
-  tag = {
-    Name = "Cloud_instance"
-    }
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "My_VPC"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "My_Subnet"
+  }
+}
+
+resource "aws_security_group" "my_sg" {
+  name   = "my_security_group"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "My_SG"
+  }
+}
+
+
+resource "aws_instance" "mera_instance" {
+  ami           = "ami-053b12d3152c0cc71"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+  tags = {
+    Name = "AWS_test"
+  }
+}
+
+
+
+
+
+
+
+/* CREATION OF INTERNET GATWAY */
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "My_Internet_Gateway"
+  }
+}
+
+
+/* CREATION OF PUBLIC SUBNET */
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true  # Ensures resources in this subnet get public IPs
+
+  tags = {
+    Name = "My_Public_Subnet"
+  }
+}
+
+
+
+/* CREATION OF NAT GATEWAY */
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "My_NAT_Gateway"
+  }
+}
+
+
+/* CREATION OF ROUT TABLE */
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "Public_Route_Table"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+/* PRIVATE ROUTE TABLE */
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "Private_Route_Table"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+/* ACL NETWORK */
+
+resource "aws_network_acl" "public_nacl" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0   # Allow all outgoing traffic
+    to_port     = 0
+
+  }
+
+  tags = {
+    Name = "Public_NACL"
+  }
+}
+
+resource "aws_network_acl_association" "public_nacl_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  network_acl_id = aws_network_acl.public_nacl.id
+}
